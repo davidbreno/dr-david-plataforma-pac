03a3eec723813da4bc3dc2174bc0fad5f634fdbd
davidbreno <dr.davidbreno@hotmail.com>
2025-10-27 21:52:41 -0300

    chore: remove proxy.ts não utilizado

--- proxy.ts
+++
@@ -1,8 +0,0 @@
-export function proxy(request: NextRequest) {
-  // Proxy logic goes here
-}
-export const config = {
-  matcher: [
-    "/((?!login|api/auth|_next/static|_next/image|favicon.ico|site.webmanifest|auth|assets).*)",
-  ],
-};
